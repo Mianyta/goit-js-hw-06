71510435fc56426a054d09d12ea81ba4a879604a
Mianyta <suta180987@gmail.com>
2023-08-15 19:51:28 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const liElements = ingredients.map(ingredient => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
+});
+
+ingredientsList.append(...liElements);
