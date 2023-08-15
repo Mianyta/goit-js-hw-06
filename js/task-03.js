617b62c43ec65938d029861c44d9adea2c80c661
Mianyta <suta180987@gmail.com>
2023-08-15 20:03:09 +0300

    task5

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,16 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const galleryList = document.querySelector(".gallery");
+
+const imagesMarkup = images.map(image => {
+  return `
+    <li class="gallery-item">
+      <img src="${image.url}" alt="${image.alt}" class="gallery-img">
+    </li>
+  `;
+}).join("");
+
+galleryList.insertAdjacentHTML("beforeend", imagesMarkup);
