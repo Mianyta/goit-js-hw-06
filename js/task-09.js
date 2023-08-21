53138382f91a24e5fae70d35a02faa196259e534
Mianyta <suta180987@gmail.com>
2023-08-21 20:37:28 +0300

    new

--- js/task-09.js
+++ js/task-09.js
@@ -1,7 +1,19 @@
+const elements = {
+  btn: document.querySelector(".change-color"),
+  body: document.querySelector("body"),
+  color: document.querySelector(".color"),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-const randomColor = getRandomHexColor();
-console.log(randomColor);
+
+elements.btn.addEventListener("click", changeColor);
+
+function changeColor(evt) {
+  let colorHex = getRandomHexColor();
+  elements.body.style.backgroundColor = colorHex;
+  elements.color.textContent = colorHex;
+}
